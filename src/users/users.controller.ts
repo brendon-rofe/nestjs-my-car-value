7b38d4f44780cb5229edb75b0717789812684aa1
Bravo1B9 <brofe93@gmail.com>
2023-05-30 15:24:45 +0000

    Initial auth service setup

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -24,7 +24,7 @@ export class UsersController {
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
     return this.usersService.create(body.email, body.password);
-  }
+  };
 
   @Get('/:id')
   async findUser(@Param('id') id: string) {
@@ -33,20 +33,20 @@ export class UsersController {
       throw new NotFoundException('User not found');
     }
     return user;
-  }
+  };
 
   @Get()
   findAllUsers(@Query('email') email: string) {
     return this.usersService.find(email);
-  }
+  };
 
   @Delete(':id')
   removeUser(@Param('id') id: string) {
     return this.usersService.remove(parseInt(id));
-  }
+  };
 
   @Patch(':id')
   updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
     return this.usersService.update(parseInt(id), body);
-  }
+  };
 }
