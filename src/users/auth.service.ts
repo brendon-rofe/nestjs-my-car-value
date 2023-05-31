91ccf46da92dcb6087f3889d8d6890f81a72946b
Bravo1B9 <brofe93@gmail.com>
2023-05-31 04:39:39 +0000

    Initial hashing of the password

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,5 +1,9 @@
 import { BadRequestException, Injectable } from "@nestjs/common";
 import { UsersService } from "./users.service";
+import { randomBytes, scrypt as _scrypt } from "crypto";
+import { promisify } from "util";
+
+const scrypt = promisify(_scrypt);
 
 @Injectable()
 export class  AuthService {
@@ -9,7 +13,15 @@ export class  AuthService {
     const users = await this.usersService.find(email);
     if(users.length) {
       throw new BadRequestException('Email in use');
-    }
+    };
+
+    const salt = randomBytes(8).toString('hex');
+
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+    const result = salt + '.' + hash.toString('hex');
+
+    
   };
 
   signin() {};
